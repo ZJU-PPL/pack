c888605d838732882dfcf9153500357f6efb657b
permui <aoyang.yu.acc@outlook.com>
2022-09-30 17:32:52 +0800

    more platforms

--- build_all.sh
+++ build_all.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/pack-win64.exe .
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/pack-windows-amd64.exe .
 
-CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o bin/pack-win32.exe .
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o bin/pack-windows-32.exe .
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o bin/pack-darwin64 .
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o bin/pack-darwin-amd64 .
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=386 go build -o bin/pack-darwin32 .
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o bin/pack-darwin-arm64 .
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/pack-linux64 .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/pack-linux-amd64 .
 
-CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o bin/pack-linux32 .
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o bin/pack-linux-arm64 .
+
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o bin/pack-linux-32 .
